655ca5e177f8b511a5eb061f8e9d6eb42efd67d3
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2022-04-10 19:15:59 +0200

    add main script
    the script can record audio files and visualize them on a plot.

---
+++ record-audio.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+
+import pyaudio
+import wave
+
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+
+def record(filename):
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+    frames = []
+    try:
+        while True:
+            data = stream.read(1024)
+            frames.append(data)
+    except KeyboardInterrupt:
+        pass
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    sound_file = wave.open(filename, "wb")
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
+
+def visualize(filename):
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    input_data = read(filename)
+    audio_read = input_data[1]
+    plt.plot(audio_read)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time")
+    plt.show()
+
+def main():
+    args = sys.argv
+    if (len(args) < 2):
+        print("no option specified")
+    elif (args[1] == "r"):
+        record(args[2])
+    elif (args[1] == "v"):
+        visualize(args[2])
+    else:
+        print("invalid option")
+
+if __name__ == "__main__":
+    main()
