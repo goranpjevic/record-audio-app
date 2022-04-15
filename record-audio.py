a4f36a0a5e5e7c213c1256c9b0736ce0dc907152
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2022-04-15 07:52:44 +0200

    feature:  add stdft
    the stdft is now calculated.
    
    two new command line arguments have been added to control the stdft.
    
    the matplotlib figure is now also bigger, and the plots are in a 2*2
    grid, so that all plots are easily visible.

--- record-audio.py
+++ record-audio.py
@@ -29,7 +29,7 @@ def record(filename, duration):
     sound_file.writeframes(b''.join(frames))
     sound_file.close()
 
-def visualize(filename, sin_frequency):
+def visualize(filename, sin_frequency, window_length, overlap):
     frame_rate, audio_read = read(filename)
     time_interval = np.arange(0,audio_read.size/frame_rate,1/frame_rate)
 
@@ -39,22 +39,26 @@ def visualize(filename, sin_frequency):
     sinusoid = np.sin(2*np.pi*sin_frequency*time_interval)
     scalar_product = np.dot(audio_read, sinusoid)
 
-    plt.rcParams["figure.figsize"] = [7.50, 5.50]
+    plt.rcParams["figure.figsize"] = [15, 8]
     plt.rcParams["figure.autolayout"] = True
-    figure, axis = plt.subplots(3)
-    axis[0].plot(time_interval, audio_read)
-    axis[0].set_title("input sound")
-    axis[0].set_ylabel("amplitude")
-    axis[0].set_xlabel("time [s]")
-    axis[1].plot(freq, np.abs(audio_dft))
-    axis[1].set_title("dft(input sound)")
-    axis[1].set_ylabel("fft amplitude |x(freq)|")
-    axis[1].set_xlabel("frequency [Hz]")
-    axis[2].plot(time_interval, sinusoid)
-    axis[2].set_title("sinusoid with frequency " + str(sin_frequency) +
+    figure, axis = plt.subplots(2, 2)
+    axis[0,0].plot(time_interval, audio_read)
+    axis[0,0].set_title("input sound")
+    axis[0,0].set_ylabel("amplitude")
+    axis[0,0].set_xlabel("time [s]")
+    axis[1,0].plot(freq, np.abs(audio_dft))
+    axis[1,0].set_title("dft(input sound)")
+    axis[1,0].set_ylabel("fft amplitude |x(freq)|")
+    axis[1,0].set_xlabel("frequency [Hz]")
+    axis[0,1].plot(time_interval, sinusoid)
+    axis[0,1].set_title("sinusoid with frequency " + str(sin_frequency) +
             "\nscalar product with input sound = " + str(scalar_product))
-    axis[2].set_ylabel("amplitude")
-    axis[2].set_xlabel("time [s]")
+    axis[0,1].set_ylabel("amplitude")
+    axis[0,1].set_xlabel("time [s]")
+    axis[1,1].specgram(audio_read, NFFT=int(window_length*frame_rate), Fs=frame_rate, noverlap=overlap*window_length/frame_rate)
+    axis[1,1].set_title("stdft(input sound)")
+    axis[1,1].set_xlabel("time [s]")
+    axis[1,1].set_ylabel("frequency [Hz]")
     plt.show()
 
 def main():
@@ -63,11 +67,13 @@ def main():
     parser.add_argument("-d", "--duration", help="duration of the recording in seconds", default=5, type=float)
     parser.add_argument("-v", "--visualize", help="visualize audio file", type=str)
     parser.add_argument("-s", "--sin-frequency", help="frequency of the sinusoid", default=1, type=float)
+    parser.add_argument("-w", "--window-length", help="window length in seconds for the stdft", default=0.1, type=float)
+    parser.add_argument("-o", "--overlap", help="window proportion overlap for the stdft", default=0, type=float)
     args = parser.parse_args()
     if (args.record != None):
         record(args.record, args.duration)
     elif (args.visualize != None):
-        visualize(args.visualize, args.sin_frequency)
+        visualize(args.visualize, args.sin_frequency, args.window_length, args.overlap)
     else:
         parser.print_help()
 
